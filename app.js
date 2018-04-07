bdd10637552c3fd4e8bd59f1da8cd772b4ffb8a3
matsta25 <matim2112@gmail.com>
2018-04-07 23:11:01 +0200

    change port env

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ app.delete('/files/:id', (req,res) => {
     })
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log("Server started on port 5000");
